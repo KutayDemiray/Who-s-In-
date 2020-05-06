590373253faf9421101354bf8ccd62bda2c4cc56
yagizyasar <53881465+yagizyasar@users.noreply.github.com>
2020-05-06 21:27:36 +0300

    User class -- not final

--- 20200429_WhosIn_v.1.0/app/src/main/java/com/example/whosin/data/LoginDataSource.java
+++ 20200429_WhosIn_v.1.0/app/src/main/java/com/example/whosin/data/LoginDataSource.java
@@ -1,6 +1,6 @@
 package com.example.whosin.data;
 
-import com.example.whosin.data.model.LoggedInUser;
+import com.example.whosin.data.model.User;
 
 import java.io.IOException;
 
@@ -9,12 +9,12 @@ import java.io.IOException;
  */
 public class LoginDataSource {
 
-    public Result<LoggedInUser> login(String username, String password) {
+    public Result<User> login(String username, String password) {
 
         try {
-            // TODO: handle loggedInUser authentication
-            LoggedInUser fakeUser =
-                    new LoggedInUser(
+            // TODO: handle user authentication
+            User fakeUser =
+                    new User(
                             java.util.UUID.randomUUID().toString(),
                             "Jane Doe");
             return new Result.Success<>(fakeUser);
--- 20200429_WhosIn_v.1.0/app/src/main/java/com/example/whosin/data/LoginRepository.java
+++ 20200429_WhosIn_v.1.0/app/src/main/java/com/example/whosin/data/LoginRepository.java
@@ -1,6 +1,6 @@
 package com.example.whosin.data;
 
-import com.example.whosin.data.model.LoggedInUser;
+import com.example.whosin.data.model.User;
 
 /**
  * Class that requests authentication and user information from the remote data source and
@@ -14,7 +14,7 @@ public class LoginRepository {
 
     // If user credentials will be cached in local storage, it is recommended it be encrypted
     // @see https://developer.android.com/training/articles/keystore
-    private LoggedInUser user = null;
+    private User user = null;
 
     // private constructor : singleton access
     private LoginRepository(LoginDataSource dataSource) {
@@ -37,17 +37,17 @@ public class LoginRepository {
         dataSource.logout();
     }
 
-    private void setLoggedInUser(LoggedInUser user) {
+    private void setUser(User user) {
         this.user = user;
         // If user credentials will be cached in local storage, it is recommended it be encrypted
         // @see https://developer.android.com/training/articles/keystore
     }
 
-    public Result<LoggedInUser> login(String username, String password) {
+    public Result<User> login(String username, String password) {
         // handle login
-        Result<LoggedInUser> result = dataSource.login(username, password);
+        Result<User> result = dataSource.login(username, password);
         if (result instanceof Result.Success) {
-            setLoggedInUser(((Result.Success<LoggedInUser>) result).getData());
+            setUser(((Result.Success<User>) result).getData());
         }
         return result;
     }
