7cf434a9770b8b76075376a773ac86bde39315f5
Kutay Demiray <kutay.demiray@ug.bilkent.edu.tr>
2020-05-07 14:07:42 +0300

    Small changes in multiple classes

--- Text Based Model/EventList.java
+++ Text Based Model/EventList.java
@@ -12,16 +12,31 @@ public class EventList implements Iterable {
    private ArrayList<Event> list;
 
    // constructors
+   /**
+    * Constructor to create empty eventlist
+    */
    public EventList() {
       list = new ArrayList<>();
-      // database
    }
-
-   public EventList( ArrayList<Event> list ) {
-      this.list = list;
+   
+//   /**
+//    * Constructor that pulls data from database to initialize list
+//    * Takes database as input
+//    */
+//   public EventList() {
+//      list = new ArrayList<>();
+//      // use database to pull data and initialize properties
+//   }
+   
+   /**
+    * Copy constructor
+    */
+   public EventList( EventList other ) {
+      list = other.list;
    }
 
    // methods   
+   
    public void addEvent( Event e ) {
       list.add( e );
    }
@@ -29,21 +44,48 @@ public class EventList implements Iterable {
    public void removeEvent( Event e ) {
       list.remove( list.indexOf(e) );
    }
-
-   public ArrayList<Event> search( String search ) {
-      ArrayList<Event> tmp = new ArrayList<>();
-      for ( Event e : list ) {
-         if ( e.getTitle().contains( search ) )
-            tmp.add( e );
-      }
-      return tmp;
-   }
-
-   public void sort() {
-
-   }
    
+   /**
+    * Iterator generator, returns list property's iterator
+    */
    public Iterator iterator() {
       return list.iterator();
    }
+   
+   // search methods
+   
+   /**
+    * Search by keyword, first considers events containing parameter in their title, then considers events containing
+    * search parameter in their description.
+    */
+   public EventList searchByKeywords( String search ) {
+      EventList tmp = new EventList();
+      
+      for ( Event e : list ) {
+         if ( e.getTitle().contains( search ) )
+            tmp.addEvent( e );
+      }
+      for( Event e : list ) {
+         if ( list.indexOf( e ) == -1 && e.getDescription().contains( search ) )
+            tmp.addEvent( e );
+      }
+      
+      return tmp;
+   }
+   
+   /**
+    * Search by 
+   
+   
+   
+   // sort methods
+   
+   /**
+    * Sorts 
+    */
+   public void sortByTitle() {
+
+   }
+   
+   
 }
