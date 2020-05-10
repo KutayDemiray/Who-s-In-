d975fc2d63e257ae2a301ab4254a79dfd409f0d4
cgtysafak <cagatay.safak@ug.bilkent.edu.tr>
2020-05-11 01:59:26 +0300

    User Adapter almost done

--- DENEMEINS/app/src/main/java/com/cgty/denemeins/Adapter/UserAdapter.java
+++ DENEMEINS/app/src/main/java/com/cgty/denemeins/Adapter/UserAdapter.java
@@ -1,6 +1,7 @@
 package com.cgty.denemeins.Adapter;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -16,6 +18,11 @@ import com.cgty.denemeins.Model.User;
 import com.cgty.denemeins.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -51,6 +58,25 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>
         holder.username.setText(user.getUsername());
         holder.nameSurname.setText(user.getNameSurname());
         Glide.with(mContext).load(user.getPpURL()).into(holder.pp);
+        isFriend(user.getId(), holder.addFriend);
+
+        if (user.getId().equals(firebaseUser.getUid()))
+            holder.addFriend.setVisibility(View.GONE);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                SharedPreferences.Editor editor;
+                editor = mContext.getSharedPreferences( "PREFS" , Context.MODE_PRIVATE).edit();
+
+                editor.putString("profileid", user.getId());
+                editor.apply();
+
+                ( (FragmentActivity) mContext).getSupportFragmentManager().beginTransaction();
+            }
+        });
     }
 
     @Override
@@ -75,5 +101,30 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>
             nameSurname = itemView.findViewById(R.id.textViewNameSurnameElement);
             addFriend = itemView.findViewById(R.id.buttonAddFriendElement);
         }
+    }
+
+    private void isFriend(final String userID, final Button button)
+    {
+        DatabaseReference friendPath;
+        friendPath = FirebaseDatabase.getInstance().getReference().child("Friend").child(firebaseUser.getUid()).child("isFriend");
+
+        friendPath.addValueEventListener(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                if (dataSnapshot.child(userID).exists())
+                    button.setText("   Added   ");                  //3 bosluk koymayinca cirkin gozukuyor button (cagatay)
+                else
+                    button.setText("   Add Friend   ");             //3 bosluk koymayinca cirkin gozukuyor button (cagatay)
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+
+            }
+        });
+
     }
 }
