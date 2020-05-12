dd75fe3393d96a02262e35e6353f752d47e9bbc1
cgtysafak <cagatay.safak@ug.bilkent.edu.tr>
2020-05-13 01:58:52 +0300

    some styling

---
+++ WHO'S IN/app/src/main/java/com/cgty/denemeins/PhotoActivity.java
@@ -0,0 +1,23 @@
+package com.cgty.denemeins;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * --- ONEMLI ---
+ *
+ * Burayi sadece foto kirpma-yukleme isini ogrenmek, test etmek ve projeye adapte etmek icin olusturdum. Simdilik bir olayi yok.
+ *
+ * @author Cagatay Safak
+ * @version 1.0
+ */
+public class PhotoActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+    }
+}
