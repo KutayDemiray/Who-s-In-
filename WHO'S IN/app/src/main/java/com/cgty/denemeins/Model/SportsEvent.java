2fa7d189fb4ecfbd60e9ca0928f38c70bf24effa
Gökberk Keskinkılıç <61826514+gokiberk@users.noreply.github.com>
2020-05-16 23:57:24 +0300

    comment toString and SportsEvent

--- WHO'S IN/app/src/main/java/com/cgty/denemeins/Model/SportsEvent.java
+++ WHO'S IN/app/src/main/java/com/cgty/denemeins/Model/SportsEvent.java
@@ -19,10 +19,12 @@ public class SportsEvent extends Event {
     private int gameType;
 
     // constructor
+    /**
     public SportsEvent( String title, User organizer, int capacity, int accessStatus, int gameType ) {
         super( title, organizer, capacity, accessStatus );
         this.gameType = gameType;
     }
+     */
 
     // methods
 
@@ -34,8 +36,9 @@ public class SportsEvent extends Event {
         this.gameType = gameType;
     }
 
+    /**
     public String toString() {
-        return "Board Game Event Title: " + getTitle() + ", Organizer: " + getOrganizerId().getNickname() + ", Capacity: " + getCapacity() + ", Privacy: " + getPrivacySetting() + ", Game Type: " + getGameType();
+        return "Board Game Event Title: " + getTitle() + ", Organizer: " + getOrganizerId().getUsername() + ", Capacity: " + getCapacity() + ", Privacy: " + getPrivacySetting() + ", Game Type: " + getGameType();
     }
-
+    */
 }
