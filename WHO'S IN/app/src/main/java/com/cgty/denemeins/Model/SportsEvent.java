b639ef3c0cad92c5153b798f72d6e34ac1f4cb29
Gökberk Keskinkılıç <61826514+gokiberk@users.noreply.github.com>
2020-05-16 23:59:09 +0300

    Revert "Merge branch 'master' of https://github.com/KutayDemiray/Who-s-In-"
    This reverts commit f066cc543689ad0f407ca53f6cb8686b0cb954f6, reversing
    changes made to 283cc4faf1a04b948f6f987e8e3a342c5ef4b960.

--- WHO'S IN/app/src/main/java/com/cgty/denemeins/Model/SportsEvent.java
+++ WHO'S IN/app/src/main/java/com/cgty/denemeins/Model/SportsEvent.java
@@ -1,7 +1,5 @@
 package com.cgty.denemeins.Model;
 
-import java.util.Date;
-
 /**
  * Sports event class, includes sports types
  * @author Kutay Demiray
@@ -20,15 +18,11 @@ public class SportsEvent extends Event {
     // properties
     private int gameType;
 
-    public SportsEvent() {
-    }
-
-    public SportsEvent(String eventId, String title, String organizerId, Date date, String description, int capacity, String mainType, String subType, String location, String privacySetting, int gameType) {
-        super(eventId, title, organizerId, date, description, capacity, mainType, subType, location, privacySetting);
+    // constructor
+    public SportsEvent( String title, User organizer, int capacity, int accessStatus, int gameType ) {
+        super( title, organizer, capacity, accessStatus );
         this.gameType = gameType;
     }
-
-    // constructor
 
     // methods
 
@@ -41,7 +35,7 @@ public class SportsEvent extends Event {
     }
 
     public String toString() {
-        return "Board Game Event Title: " + getTitle() + ", Organizer: " + getOrganizerId() + ", Capacity: " + getCapacity() + ", Privacy: " + getPrivacySetting() + ", Game Type: " + getGameType();
+        return "Board Game Event Title: " + getTitle() + ", Organizer: " + getOrganizerId().getNickname() + ", Capacity: " + getCapacity() + ", Privacy: " + getPrivacySetting() + ", Game Type: " + getGameType();
     }
 
 }
