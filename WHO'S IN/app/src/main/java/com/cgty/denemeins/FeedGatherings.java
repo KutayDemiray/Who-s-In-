24bb31c990439d8c510c03a393128245910c1716
cgtysafak <cagatay.safak@ug.bilkent.edu.tr>
2020-05-17 22:17:33 +0300

    .

---
+++ WHO'S IN/app/src/main/java/com/cgty/denemeins/FeedGatherings.java
@@ -0,0 +1,14 @@
+package com.cgty.denemeins;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FeedGatherings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_gatherings);
+    }
+}
