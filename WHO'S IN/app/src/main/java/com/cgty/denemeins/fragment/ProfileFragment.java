1ac0ddb02b6cede222ff92345c838745c13b222a
cgtysafak <cagatay.safak@ug.bilkent.edu.tr>
2020-05-18 23:19:17 +0300

    Update ProfileFragment.java

--- WHO'S IN/app/src/main/java/com/cgty/denemeins/fragment/ProfileFragment.java
+++ WHO'S IN/app/src/main/java/com/cgty/denemeins/fragment/ProfileFragment.java
@@ -1,8 +1,5 @@
 package com.cgty.denemeins.fragment;
 
-/**
- * @author Gökberk Keskinkılıç
- */
 import com.bumptech.glide.Glide;
 import com.cgty.denemeins.LoginActivity;
 import com.cgty.denemeins.R;
@@ -55,6 +52,11 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 import static android.app.Activity.RESULT_OK;
 
+/**
+ * Profile Fragment Class
+ * @author Gökberk Keskinkılıç, Cagatay Safak
+ * @version 2.0
+ */
 public class ProfileFragment extends Fragment
 {
     //properties
@@ -138,178 +140,42 @@ public class ProfileFragment extends Fragment
             }
         });
 
-        private void userInfo()
-        {
-            DatabaseReference userPath;
-            userPath = FirebaseDatabase.getInstance().getReference("Users").child(profileID);
-
-            userPath.addValueEventListener(new ValueEventListener()
-            {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot)
-                {
-
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError)
-                {
-
-                }
-            });
-        }
-
-        //goko
-
-        storageReference = FirebaseStorage.getInstance().getReference("Uploads");
-
-        reference = FirebaseDatabase.getInstance().getReference( "Users").child(currentUser.getUid());
-
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                User user = dataSnapshot.getValue(User.class);
-                username.setText(user.getUsername());
-                if( user.getImageURL().equals("default"))
-                    image_profile.setImageResource(R.mipmap.ic_launcher);
-                else
-                    Glide.with(getContext()).load(user.getImageURL()).into(image_profile);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-        image_profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openImage();
-            }
-        });
-
-        setupFireBaseListener();
-
-        mSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d( TAG, "onClick: attempting to sign out the use.");
-                FirebaseAuth.getInstance().signOut();
-            }
-        });
-
         return view;
     }
 
-    private void openImage() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult( intent, IMAGE_REQUEST);
-    }
+    private void userInfo()
+    {
+        DatabaseReference userPath;
+        userPath = FirebaseDatabase.getInstance().getReference("Users").child(profileID);
 
-    private String getFileExtension(Uri uri){
-        ContentResolver contentResolver = getContext().getContentResolver();
-        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
-        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
-    }
-
-    private void uploadImage(){
-        final ProgressDialog pd = new ProgressDialog(getContext());
-        pd.setMessage("Uploading...");
-        pd.show();
-
-        if ( imageUri != null){
-            final StorageReference fileReference = storageReference.child(System.currentTimeMillis()
-            +"-"+getFileExtension(imageUri));
-
-            uploadTask = fileReference.getFile(imageUri);
-            uploadTask.continueWith(new Continuation <UploadTask.TaskSnapshot, Task<Uri>>() {
-                @Override
-                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
-                    if( !task.isSuccessful())
-                        throw task.getException();
-                    return fileReference.getDownloadUrl();
-                }
-            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
-                @Override
-                public void onComplete(@NonNull Task<Uri> task) {
-                    if( task.isSuccessful()){
-                        Uri downloadUri = task.getResult();
-                        String mUri = downloadUri.toString();
-
-                        reference = FirebaseDatabase.getInstance().getReference("Users").child(currentUser.getUid());
-                        HashMap<String, Object> map = new HashMap<>();
-                        map.put("imageURL", mUri);
-                        reference.updateChildren(map);
-
-                        pd.dismiss();
-                    }
-                    else{
-                        Toast.makeText( getContext(), "Failed!", Toast.LENGTH_SHORT).show();
-                        pd.dismiss();
-                    }
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-                    pd.dismiss();
-                }
-            });
-        }
-        else{
-            Toast.makeText(getContext(), "No image selected", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
-    public void onActivityResult( int requestCode, int resultCode, Intent data){
-        super.onActivityResult( requestCode, resultCode, data );
-
-        if( requestCode == IMAGE_REQUEST && resultCode == RESULT_OK && data != null){
-            imageUri = data.getData();
-
-            if ( uploadTask != null && uploadTask.isInProgress())
-                Toast.makeText(getContext(), "Upload is in progress", Toast.LENGTH_SHORT).show();
-            else{
-                uploadImage();
-            }
-        }
-    }
-
-    private void setupFireBaseListener(){
-        Log.d( TAG, "setupFirebaseListener: setting up the auth state listener.");
-        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+        userPath.addValueEventListener(new ValueEventListener()
+        {
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if( user != null){
-                    Log.d( TAG, "onAuthStateChanged: signed_in: " + user.getUid());
-                }
-                else{
-                    Log.d( TAG, "onAuthStateChanged: signed_out");
-                    Toast.makeText( getActivity(), "Signed out!", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent( getActivity(), LoginActivity.class);
-                    intent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity( intent);
-                }
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                if (getContext() == null)
+                    return;
+
+                User user;
+                user = dataSnapshot.getValue(User.class);
+
+                Glide.with(getContext()).load(user.getImageURL()).into(image_profile);
+
+                textView_Username.setText(user.getUsername());
+                textView_Age.setText(user.getAge());
+                textView_Bio.setText(user.getBio());
             }
-        };
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+
+            }
+        });
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseAuth.getInstance().addAuthStateListener(mAuthStateListener);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if( mAuthStateListener != null){
-            FirebaseAuth.getInstance().removeAuthStateListener(mAuthStateListener);
-        }
+    private void followController()
+    {
+        DatabaseReference followPath = FirebaseDatabase.getInstance().getReference().child("Follow").child(currentUser.getUid()).child("following");
     }
 }
