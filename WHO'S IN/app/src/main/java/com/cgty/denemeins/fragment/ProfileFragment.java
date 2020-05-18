82f78f9ab92fc0b142b61027ef5bd5c4ca9b3740
cgtysafak <cagatay.safak@ug.bilkent.edu.tr>
2020-05-19 00:42:01 +0300

    comment for gokberk

--- WHO'S IN/app/src/main/java/com/cgty/denemeins/fragment/ProfileFragment.java
+++ WHO'S IN/app/src/main/java/com/cgty/denemeins/fragment/ProfileFragment.java
@@ -161,7 +161,8 @@ public class ProfileFragment extends Fragment
 
         setupFireBaseListener();
 
-        mSignOut.setOnClickListener(new View.OnClickListener() {
+        mSignOut.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View v) {
                 Log.d( TAG, "onClick: attempting to sign out the use.");
@@ -288,6 +289,9 @@ public class ProfileFragment extends Fragment
                     if (event.getOrganizerId().equals(profileID))
                         i++;
                 }
+
+                textView_
+
             }
 
             @Override
@@ -296,6 +300,262 @@ public class ProfileFragment extends Fragment
 
             }
         });
+    }
+
+    private void setupFireBaseListener()
+    {
+        Log.d( TAG, "setupFirebaseListener: setting up the auth state listener.");
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener()
+        {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth)
+            {
+                FirebaseUser user;
+                user = firebaseAuth.getCurrentUser();
+
+                if ( user != null)
+                    Log.d( TAG, "onAuthStateChanged: signed_in: " + user.getUid());
+                else
+                {
+                    Log.d( TAG, "onAuthStateChanged: signed_out");
+                    Toast.makeText( getActivity(), "Signed out!", Toast.LENGTH_SHORT).show();
+
+                    Intent intent;
+                    intent = new Intent( getActivity(), LoginActivity.class);
+                    intent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                    startActivity( intent);
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+
+        if( mAuthStateListener != null)
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthStateListener);
+    }
+
+    /**
+    TextView username;
+    DatabaseReference reference;
+    StorageReference storageReference;
+    private static final int IMAGE_REQUEST = 1;
+    private Uri imageUri;
+    private StorageTask uploadTask;
+    private static final String TAG = "SETTINGS";
+
+    //firebase
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    //constructor
+    public ProfileFragment()
+    {
+        //required empty public constructor.
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View view;
+        view = inflater.inflate( R.layout.fragment_profile, container, false);
+
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        SharedPreferences prefs = getContext().getSharedPreferences("PREFS", Context.MODE_PRIVATE);
+        profileID = prefs.getString("profileid","none");
+
+        mSignOut = (ImageButton) view.findViewById( R.id.buttonSignOut);
+        image_profile = view.findViewById(R.id.profilePicture);
+
+        textView_Age = view.findViewById(R.id.textViewProfileAge);
+        textView_Username = view.findViewById(R.id.textViewProfileUsername);                                          //id gonna be changed
+        textView_Bio = view.findViewById(R.id.textViewProfileBio);
+
+        button_EditProfile = view.findViewById(R.id.buttonEditProfile_profile);
+        button_Followers = view.findViewById(R.id.buttonFollowers_profile);
+        button_Following = view.findViewById(R.id.buttonFollowing_profile);
+        button_PastActivities = view.findViewById(R.id.buttonPastActivities_profile);
+
+        button_EditProfile.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View v)
+            {
+                String buttonText;
+                buttonText = button_EditProfile.getText().toString();
+
+                if ( buttonText.equals( "EDIT PROFILE"))
+                {
+                    //go to edit profile screen
+                }
+                else if ( buttonText.equals( "FOLLOW"))
+                {
+                    FirebaseDatabase.getInstance().getReference().child("Follow").child(currentUser.getUid()).child("following").child(profileID).setValue(true);
+                    FirebaseDatabase.getInstance().getReference().child("Follow").child(profileID).child("followers").child(currentUser.getUid()).setValue(true);
+                }
+                else if ( buttonText.equals( "FOLLOWING"))
+                {
+                    FirebaseDatabase.getInstance().getReference().child("Follow").child(currentUser.getUid()).child("following").child(profileID).removeValue();
+                    FirebaseDatabase.getInstance().getReference().child("Follow").child(profileID).child("followers").child(currentUser.getUid()).removeValue();
+                }
+            }
+        });
+
+        private void userInfo()
+        {
+            DatabaseReference userPath;
+            userPath = FirebaseDatabase.getInstance().getReference("Users").child(profileID);
+
+            userPath.addValueEventListener(new ValueEventListener()
+            {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+                {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError)
+                {
+
+                }
+            });
+        }
+
+        //goko
+
+        storageReference = FirebaseStorage.getInstance().getReference("Uploads");
+
+        reference = FirebaseDatabase.getInstance().getReference( "Users").child(currentUser.getUid());
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                username.setText(user.getUsername());
+                if( user.getImageURL().equals("default"))
+                    image_profile.setImageResource(R.mipmap.ic_launcher);
+                else
+                    Glide.with(getContext()).load(user.getImageURL()).into(image_profile);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+        image_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImage();
+            }
+        });
+
+        setupFireBaseListener();
+
+        mSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d( TAG, "onClick: attempting to sign out the use.");
+                FirebaseAuth.getInstance().signOut();
+            }
+        });
+
+        return view;
+    }
+
+    private void openImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult( intent, IMAGE_REQUEST);
+    }
+
+    private String getFileExtension(Uri uri){
+        ContentResolver contentResolver = getContext().getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadImage(){
+        final ProgressDialog pd = new ProgressDialog(getContext());
+        pd.setMessage("Uploading...");
+        pd.show();
+
+        if ( imageUri != null){
+            final StorageReference fileReference = storageReference.child(System.currentTimeMillis()
+                    +"-"+getFileExtension(imageUri));
+
+            uploadTask = fileReference.getFile(imageUri);
+            uploadTask.continueWith(new Continuation <UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if( !task.isSuccessful())
+                        throw task.getException();
+                    return fileReference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if( task.isSuccessful()){
+                        Uri downloadUri = task.getResult();
+                        String mUri = downloadUri.toString();
+
+                        reference = FirebaseDatabase.getInstance().getReference("Users").child(currentUser.getUid());
+                        HashMap<String, Object> map = new HashMap<>();
+                        map.put("imageURL", mUri);
+                        reference.updateChildren(map);
+
+                        pd.dismiss();
+                    }
+                    else{
+                        Toast.makeText( getContext(), "Failed!", Toast.LENGTH_SHORT).show();
+                        pd.dismiss();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    pd.dismiss();
+                }
+            });
+        }
+        else{
+            Toast.makeText(getContext(), "No image selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onActivityResult( int requestCode, int resultCode, Intent data){
+        super.onActivityResult( requestCode, resultCode, data );
+
+        if( requestCode == IMAGE_REQUEST && resultCode == RESULT_OK && data != null){
+            imageUri = data.getData();
+
+            if ( uploadTask != null && uploadTask.isInProgress())
+                Toast.makeText(getContext(), "Upload is in progress", Toast.LENGTH_SHORT).show();
+            else{
+                uploadImage();
+            }
+        }
     }
 
     private void setupFireBaseListener(){
@@ -318,6 +578,8 @@ public class ProfileFragment extends Fragment
         };
     }
 
+
+
     @Override
     public void onStart() {
         super.onStart();
@@ -331,4 +593,6 @@ public class ProfileFragment extends Fragment
             FirebaseAuth.getInstance().removeAuthStateListener(mAuthStateListener);
         }
     }
+}
+     */
 }
