89f5ea537d5d159b5e03e73823d5963c9eef5b23
kaanozaltan <63501746+kaanozaltan@users.noreply.github.com>
2020-05-20 14:12:07 +0300

    .

--- WHO'S IN/app/src/main/java/com/cgty/denemeins/ShowPastEvents.java
+++ WHO'S IN/app/src/main/java/com/cgty/denemeins/ShowPastEvents.java
@@ -7,9 +7,9 @@ import androidx.annotation.Nullable;
 
 public class ShowPastEvents extends Activity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
 
-        setContentView( R.layout.show_past_events);
+        setContentView( R.layout.show_past_events );
     }
 }
